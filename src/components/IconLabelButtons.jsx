64f4c59581765ffcff0dd49b77369eea383818b2
Sohaib Manah <souhaibemanah@gmail.com>
2023-01-26 14:12:45 +0100

    add video and audio support

--- src/components/IconLabelButtons.jsx
+++ src/components/IconLabelButtons.jsx
@@ -26,31 +26,36 @@ export default function IconLabelButtons(props) {
       // );
       const formData = new FormData();
       props.imgs.forEach((file) => formData.append(file.name, file.imgData));
-      const res = await fetch("http://localhost:8080/api/v1/upload", {
-        method: "POST",
-        body: formData,
-      });
+      const res = await fetch(
+        "http://localhost:8080/api/v1/uploads?categorie=images", //images or video or audio
+        {
+          method: "POST",
+          body: formData,
+        }
+      );
       const data = await res.json();
+      // console.log(
+      //   "🚀 ~ file: IconLabelButtons.jsx:33 ~ sendHanlder ~ res",
+      //   res
+      // );
+
+      if (data.status === "fail")
+        throw new Error(data.message + " " + "<server error>");
+      props.onHide({
+        type: "success",
+        message: "Sent",
+      });
       console.log(
-        "🚀 ~ file: IconLabelButtons.jsx:34 ~ sendHanlder ~ data",
+        "🚀 ~ file: IconLabelButtons.jsx:30 ~ sendHanlder ~ data",
         data
       );
-      if (data.status === "fail") {
-        props.onHide({ type: "error", message: data.data.error });
-      } else {
-        props.onHide({
-          type: "success",
-          message: "Sent",
-        });
-      }
 
       //todo add error handling
       //todo add a spinner
       //?info  the message of confirmation is not showing
     } catch (e) {
       console.log(e);
-      // if (data.status === "fail")
-      // return props.onHide({ typeof: "error", message: data.message });
+      return props.onHide({ type: "error", message: e.message });
     }
   };
   return (
